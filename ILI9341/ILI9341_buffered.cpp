ab2ea9056f7be78b4a37bf8dbde32249db873de7
Daan Boezeman <boezemandaan@gmail.com>
2019-07-01 15:31:38 +0200

    added boost license

--- ILI9341/ILI9341_buffered.cpp
+++ ILI9341/ILI9341_buffered.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright Daan Boezeman 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 #include "ILI9341_buffered.hpp"
 
 using namespace hwlib;
